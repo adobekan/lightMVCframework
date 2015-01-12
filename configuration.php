4ebd4ee388f2a77619f11adc4d9393d400a8ce98
adobekan <ado_bekan@hotmail.com>
2015-01-12 15:07:19 +0100

    system parameters

--- configuration.php
+++ configuration.php
@@ -1,3 +1,28 @@
 <?php
-
+/*  System parametars
+ *  - webpage
+ *  - database (MySQL)
+ *  - other
+ */
+ 
+ /* Webpage parameters*/
+ define('PAGE_NAME','');
+ define('EMAIL','');
+ define('VERSION','');
+ define('DEBUG', false);
+ 
+ /* database connection parameters*/
+ define('HOSTNAME','localhost');
+ define('DB_CHARSET', 'utf8');
+ define('DB_NAME','');
+ define('DB_USER','');
+ define('DB_PASS','');
+ define('TABLE_PREFIX','lig_');
+ 
+ /*other parameters*/
+ 
+ //http://php.net/manual/en/timezones.php 
+ define('TIMEZONE','Europe/Ljubljana'); 
+ define('SMTP','mail.gmail.com'); 
+ define('SMTP_PORT','25');
 ?>
