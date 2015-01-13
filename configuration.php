361e6d83ae4e47a0eeee72b44d7e0495fe74cd8d
adobekan <ado_bekan@hotmail.com>
2015-01-13 16:42:39 +0100

    minor update

--- configuration.php
+++ configuration.php
@@ -1,28 +1,29 @@
 <?php
 /*  System parametars
- *  - webpage
- *  - database (MySQL)
- *  - other
- */
- 
- /* Webpage parameters*/
- define('PAGE_NAME','');
- define('EMAIL','');
- define('VERSION','');
- define('DEBUG', false);
- 
- /* database connection parameters*/
- define('HOSTNAME','localhost');
- define('DB_CHARSET', 'utf8');
- define('DB_NAME','');
- define('DB_USER','');
- define('DB_PASS','');
- define('TABLE_PREFIX','lig_');
- 
- /*other parameters*/
- 
- //http://php.net/manual/en/timezones.php 
- define('TIMEZONE','Europe/Ljubljana'); 
- define('SMTP','mail.gmail.com'); 
- define('SMTP_PORT','25');
-?>
+*  - webpage
+*  - database (MySQL)
+*  - other
+*/
+
+/* Webpage parameters*/
+define('PAGE_NAME','');
+define('EMAIL','');
+define('VERSION','');
+define('DEBUG', false);
+define('BASE_PATH', dirname(realpath(__FILE__)));
+define('DS', DIRECTORY_SEPARATOR);
+
+/* database connection parameters*/
+define('HOSTNAME','localhost');
+define('DB_CHARSET', 'utf8');
+define('DB_NAME','');
+define('DB_USER','');
+define('DB_PASS','');
+define('TABLE_PREFIX','lig_');
+
+/*other parameters*/
+
+//http://php.net/manual/en/timezones.php
+define('TIMEZONE','Europe/Ljubljana');
+define('SMTP','mail.gmail.com');
+define('SMTP_PORT','25');
---
+++ content/index.php
@@ -0,0 +1,4 @@
+<?php
+include_once('../configuration.php');
+$url=$_GET['url'];
+require_once(ROOT .DS. 'library' .DS. bootstrap.php );
